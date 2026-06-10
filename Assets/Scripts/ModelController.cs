3c86362e178f0fe3e0355a3354c027fa6feaa1fd
Pedro Gabriel <pedro.goncalves@edu.vertex.org.br>
2026-06-10 18:14:32 -0300

    Implementa escala, visibilidade e reset no ModelController

--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -7,21 +7,35 @@ public class ModelController : MonoBehaviour
     [Tooltip("Transform do modelo 3D que será manipulado")]
     [SerializeField] private Transform modelTransform;
 
-    [Tooltip("Renderer utilizado para controlar a visibilidade do modelo")]
-    [SerializeField] private MeshRenderer modelRenderer;
+    private Vector3 initialScale;
+
+    void Start()
+    {
+        initialScale = modelTransform.localScale;
+
+    }
 
     public void SetScale(float value)
     {
+        value = Mathf.Clamp(value, 0.5f, 5.0f);
 
+        modelTransform.localScale = new Vector3(value, value, value);
     }
 
     public void SetVisibility(bool visible)
     {
+        MeshRenderer[] renderers = modelTransform.GetComponentsInChildren<MeshRenderer>();
 
+        foreach (MeshRenderer renderer in renderers)
+        {
+            renderer.enabled = visible;
+        }
     }
 
     public void ResetModel()
     {
+        modelTransform.localScale = initialScale;
 
+        SetVisibility(true);
     }
 }
